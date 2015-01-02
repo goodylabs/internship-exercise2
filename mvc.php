d5a87d82b5454160aaa05e90a7924650d69d69f2
eric2861994 <eric2861994@gmail.com>
2015-01-02 23:25:42 +0700

    Standarized HTML

--- mvc.php
+++ mvc.php
@@ -8,8 +8,10 @@ class View
     public function __construct($controller, $model) {
         $this->controller = $controller;
         $this->model = $model;
-        $this->part1 = '<html>
+        $this->part1 = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
+<html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <title>Fuel Calculator</title>
 </head>
 <body>
